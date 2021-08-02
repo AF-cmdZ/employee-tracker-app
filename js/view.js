6928619a0afc1d42a929125dd939117d814afd87
A Fish <ashley.paz.fish@gmail.com>
2021-08-02 17:07:21 -0500

    viewAllEmployees.function()

--- js/view.js
+++ js/view.js
@@ -13,3 +13,20 @@ console.log(cTable);
 console.log(mysql);
 console.log(inquirer);
 // End temporary
+
+module.exports = {
+    viewAllEmployees: function (cb) {
+        connection.query(
+            'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department, concat(manager.first_name, " ", manager.last_name) as Manager \
+            FROM employee as employee INNER JOIN role as role on employee.role_id = role.id \
+            INNER JOIN department as department ON role.department_id = department.id \
+            LEFT JOIN employee as manager on employee.manager_id = manager.id ORDER BY employee.id;',
+            (err, results) => {
+                if (err) throw err;
+                console.table(results);
+                cb(results);
+            }
+        );
+    },
+    viewAllRoles:
+}
