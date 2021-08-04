22886348335f453f9bbd895398b23adffb3605d9
A Fish <ashley.paz.fish@gmail.com>
2021-08-04 16:28:55 -0500

    addDepartment.function() and addRole.function()

--- js/add.js
+++ js/add.js
@@ -13,3 +13,28 @@ console.log(cTable);
 console.log(mysql);
 console.log(inquirer);
 // End temporary
+
+module.exports = {
+    addDepartment: function (newDept, cb) {
+        connection.query(
+            "INSERT INTO department (name) VALUES (?);",
+            newDept,
+            (err, res) => {
+                if (err) throw err;
+                console.log("Department has been added! \n");
+                cb(res);
+            }
+        );
+    },
+    addRole: function (title, salary, departmentId, cb) {
+        connection.query(
+            "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);",
+            [title, salary, departmentId],
+            (err, res) => {
+                if (err) throw err;
+                console.log("role has been added to the database! \n");
+                cb(res);
+            }
+        );
+    },
+};
