cf85dabf426a7d1c01a8507b1c958aa299871f96
A Fish <ashley.paz.fish@gmail.com>
2021-08-05 18:04:37 -0500

    deleteDept.function()

--- js/delete.js
+++ js/delete.js
@@ -2,14 +2,14 @@ import inquirer from "inquirer";
 const mysql = require("mysql2");
 import connection from "./config/connection.js";
 
-module.exports {
+module.exports = {
     deleteEmp: function (eeName, cb) {
         inquirer.prompt([
             {
                 name: "selectEmp",
                 type: "list",
                 choices: eeName,
-                message: "Select the employee to be deleted:",
+                message: "Select the employee to be deleted:"
             },
         ])
         .then((answers) => {
@@ -27,15 +27,14 @@ module.exports {
                 }
             );
         });
-    }
-    {
+    },
         deleteRole: function (rrName, cb) {
             inquirer.prompt([
                 {
                     name: "selectR",
                     type: "list",
                     choices: rrName,
-                    message: "Select the role to be deleted:",
+                    message: "Select the role to be deleted:"
                 },
             ])
             .then((answers) => {
@@ -52,10 +51,29 @@ module.exports {
                     }
                 );
             });
-        }
-        {
-            delet
-        }   
-    }
+        },
+            deleteDept: function (dName, cb) {
+                inquirer.prompt([
+                    {
+                    name: "selectD",
+                    type: "list",
+                    choices: dName,
+                    message: "Select the department to delete:"
+                    },
+                ])
+                .then((answers) => {
+                    let deptId = answers.selectD.split(" ");
+                    let dID = deptId[0];
 
+                    connection.query(
+                        "DELETE FROM department WHERE id = ?;",
+                        [dID],
+                        (err, results) => {
+                            if (err) throw err;
+                            console.log("Department has been deleted.");
+                            cb(results);
+                        }
+                    );
+                });
+            },
 };
