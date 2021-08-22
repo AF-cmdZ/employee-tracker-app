4486732a22332c57ae25b719780d2ffa6390b220
A Fish <ashley.paz.fish@gmail.com>
2021-08-22 11:52:17 -0500

    delete console logs

--- js/view.js
+++ js/view.js
@@ -1,19 +1,9 @@
-import { config } from "dotenv";
 import inquirer from "inquirer";
 const mysql = require("mysql2");
 import prompt from "inquirer";
 import connection from "./config/connection.js";
 const cTable = require("console.table");
 
-// Setup my import variables so they dont delete.
-// THIS IS TEMPORARY
-console.log(config);
-console.log(connection);
-console.log(prompt);
-console.log(cTable);
-console.log(mysql);
-console.log(inquirer);
-// End temporary
 
 module.exports = {
     viewAllEmployees: function (cb) {
